6a2bdfc8ae7d6bdf2ad886a6cb7e4acd24a5d68c
Li Jie <cpunion@gmail.com>
2024-11-03 10:49:42 +0800

    add Long tests

--- long.go
+++ long.go
@@ -18,18 +18,6 @@ func MakeLong(i int64) Long {
 	return newLong(C.PyLong_FromLongLong(C.longlong(i)))
 }
 
-func (l Long) Int64() int64 {
-	return int64(C.PyLong_AsLongLong(l.obj))
-}
-
-func (l Long) Uint64() uint64 {
-	return uint64(C.PyLong_AsUnsignedLongLong(l.obj))
-}
-
-func (l Long) AsFloat64() float64 {
-	return float64(C.PyLong_AsDouble(l.obj))
-}
-
 func LongFromFloat64(v float64) Long {
 	return newLong(C.PyLong_FromDouble(C.double(v)))
 }
@@ -45,12 +33,27 @@ func LongFromUnicode(u Object, base int) Long {
 	return newLong(C.PyLong_FromUnicodeObject(u.Obj(), C.int(base)))
 }
 
-func (l Long) AsUint64() uint64 {
+func (l Long) Int() int {
+	return int(l.Int64())
+}
+
+func (l Long) Int64() int64 {
+	return int64(C.PyLong_AsLongLong(l.obj))
+}
+
+func (l Long) Uint() uint {
+	return uint(l.Uint64())
+}
+
+func (l Long) Uint64() uint64 {
 	return uint64(C.PyLong_AsUnsignedLongLong(l.obj))
 }
 
-func (l Long) AsUintptr() uintptr {
-	return uintptr(C.PyLong_AsLong(l.obj))
+func (l Long) Uintptr() uintptr {
+	return uintptr(l.Int64())
+}
+func (l Long) Float64() float64 {
+	return float64(C.PyLong_AsDouble(l.obj))
 }
 
 func LongFromUintptr(v uintptr) Long {
