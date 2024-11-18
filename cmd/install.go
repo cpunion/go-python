7f5317e7c9e5e0fd7b49b12111f5a085bf57315f
Li Jie <cpunion@gmail.com>
2024-11-18 10:48:04 +0800

    gopy install

--- cmd/install.go
+++ cmd/install.go
@@ -1,40 +1,37 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/cpunion/go-python/cmd/internal/rungo"
 	"github.com/spf13/cobra"
 )
 
 // installCmd represents the install command
 var installCmd = &cobra.Command{
-	Use:   "install",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Use:   "install [flags] [packages]",
+	Short: "Install Go packages with Python environment configured",
+	Long: `Install compiles and installs Go packages with the Python environment properly configured.
+If package is a directory, it will be used directly. Otherwise, the directory
+containing the package will be determined.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Example:
+  gopy install .
+  gopy install -v ./cmd/myapp
+  gopy install -v -race .`,
+	DisableFlagParsing: true, // Let go install handle all flags
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("install called")
+		if err := rungo.RunGoCommand("install", args); err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(installCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// installCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// installCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
